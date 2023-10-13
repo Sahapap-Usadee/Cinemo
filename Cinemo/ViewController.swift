22afeee9255952939fafa98c70cf281833deac62
Sahapap <sahapap@siamsquared.com>
2023-10-13 10:21:39 +0700

    init project

---
+++ Cinemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinemo
+//
+//  Created by sahapap usadee on 13/10/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
